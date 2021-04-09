c28d0c87943d8155e1694f5b2ae17a29a0e35556
tan <tan10a7@gmail.com>
2021-04-09 21:33:47 +0700

    add 3

--- pages/cauhoi/[id].js
+++ pages/cauhoi/[id].js
@@ -53,11 +53,9 @@ const Ques = ({result,isLogin,result2,avatar})=>{
           key="og:title"
         />
         <meta property="og:url" key="og:url" content="arc.com/customsoftware" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/latest.js?config=AM_CHTML"></script>
      
       </Head>
-      <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3.0.1/es5/tex-mml-chtml.js"></script>
+  
         <Question isLogin={isLogin} result={result} result2={result2} avatar={avatar}></Question>
     </div>)
 }
